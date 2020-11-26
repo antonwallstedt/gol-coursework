fab9c8fad1b87669a2f278ab800a843a6ab81c56
antonwallstedt <antonwallstedt@outlook.com>
2020-11-26 15:54:19 +0000

    Finished step 1
    Program can now run Game of Life on a single thread.

--- parallel/gol/distributor.go
+++ parallel/gol/distributor.go
@@ -1,24 +1,128 @@
 package gol
 
+import (
+	"fmt"
+
+	"uk.ac.bris.cs/gameoflife/util"
+)
+
+const ALIVE = 255
+const DEAD = 0
+
 type distributorChannels struct {
-	events    chan<- Event
-	ioCommand chan<- ioCommand
-	ioIdle    <-chan bool
+	events     chan<- Event
+	ioCommand  chan<- ioCommand
+	ioIdle     <-chan bool
+	ioFilename chan<- string
+	ioInput    <-chan uint8
+	ioOutput   chan<- uint8
 }
 
-// distributor divides the work between workers and interacts with other goroutines.
+/* mod : Calculates the remainder of a given number x when divided by m.
+Doesn't allow negative numbers so m is added to x before computing mod. */
+func mod(x, m int) int {
+	return (x + m) % m
+}
+
+/* calculateNeighbours : Counts the number of alive neighbours around a given cell.
+Does it in a closed domain, i.e. the top-most pixels are connected to the bottom,
+and left-most pixels are connected to the right-most pixels and vice versa. */
+func calculateNeighbours(p Params, x, y int, world [][]byte) int {
+	neighbours := 0
+	for i := -1; i <= 1; i++ {
+		for j := -1; j <= 1; j++ {
+			if i != 0 || j != 0 {
+				if world[mod(y+i, p.ImageHeight)][mod(x+j, p.ImageWidth)] == ALIVE {
+					neighbours++
+				}
+			}
+		}
+	}
+	return neighbours
+}
+
+/* calculateNextState : completes one whole evolution of the game and returns the new state */
+func calculateNextState(p Params, world [][]byte) [][]byte {
+	newWorld := make([][]byte, p.ImageHeight)
+	for i := range newWorld {
+		newWorld[i] = make([]byte, p.ImageWidth)
+	}
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			neighbours := calculateNeighbours(p, x, y, world)
+			if world[y][x] == ALIVE {
+				if neighbours == 2 || neighbours == 3 {
+					newWorld[y][x] = ALIVE
+				} else {
+					newWorld[y][x] = DEAD
+				}
+			} else {
+				if neighbours == 3 {
+					newWorld[y][x] = ALIVE
+				} else {
+					newWorld[y][x] = DEAD
+				}
+			}
+		}
+	}
+	return newWorld
+}
+
+/* calculateAliveCells : locates alive cells in a given world and returns a slice containing coordinates to those cells */
+func calculateAliveCells(p Params, world [][]byte) []util.Cell {
+	aliveCells := []util.Cell{}
+	for y := range world {
+		for x := range world {
+			if world[y][x] == ALIVE {
+				aliveCells = append(aliveCells, util.Cell{X: x, Y: y})
+			}
+		}
+	}
+	return aliveCells
+}
+
+/* distributor : Divides the work between workers and interacts with other goroutines. */
 func distributor(p Params, c distributorChannels) {
+	/* 	Explanatory comment
+	*
+	* We start with sending the ioInput command (which is an enum with a specific int value) down the command channel.
+	* startIO (in gol.go line 34) will start the IO goroutine, in which a never-ending for loop is running,
+	* this loop is used in conjunction with a select statement (in io.go line 124 - 143), which means whenever a command is
+	* sent down the ioCommand channel, this select statement will pick up that command and go into one of the three cases.
+	*
+	* In the case of ioInput, the readPgmImage() function inside io.go will be waiting for a filename, that's why
+	* we send the filename down the ioFilename channel, and the filename channel in the distributor is send only.
+	*
+	 */
+	c.ioCommand <- ioInput
+	c.ioFilename <- fmt.Sprintf("%vx%v", p.ImageHeight, p.ImageWidth)
 
 	// TODO: Create a 2D slice to store the world.
 	// TODO: For all initially alive cells send a CellFlipped Event.
+	world := make([][]byte, p.ImageHeight)
+	for i := range world {
+		world[i] = make([]byte, p.ImageWidth)
+	}
 
-	turn := 0
+	// Load in cells into our new world from a given file
+	for y := range world {
+		for x := range world {
+			world[y][x] = <-c.ioInput
+		}
+	}
 
 	// TODO: Execute all turns of the Game of Life.
 	// TODO: Send correct Events when required, e.g. CellFlipped, TurnComplete and FinalTurnComplete.
 	//		 See event.go for a list of all events.
+	turn := 0
+	for turn < p.Turns {
+		world = calculateNextState(p, world)
+		turn++
+	}
+	aliveCells := calculateAliveCells(p, world)
+	c.events <- FinalTurnComplete{CompletedTurns: turn, Alive: aliveCells}
 
-	// Make sure that the Io has finished any output before exiting.
+	// Make sure that the IO has finished any output before exiting.
 	c.ioCommand <- ioCheckIdle
 	<-c.ioIdle
 
--- parallel/gol/gol.go
+++ parallel/gol/gol.go
@@ -13,20 +13,26 @@ func Run(p Params, events chan<- Event, keyPresses <-chan rune) {
 
 	ioCommand := make(chan ioCommand)
 	ioIdle := make(chan bool)
+	ioFilename := make(chan string)
+	ioOutput := make(chan uint8)
+	ioInput := make(chan uint8)
 
 	distributorChannels := distributorChannels{
 		events,
 		ioCommand,
 		ioIdle,
+		ioFilename,
+		ioInput,
+		ioOutput,
 	}
 	go distributor(p, distributorChannels)
 
 	ioChannels := ioChannels{
 		command:  ioCommand,
 		idle:     ioIdle,
-		filename: nil,
-		output:   nil,
-		input:    nil,
+		filename: ioFilename,
+		output:   ioOutput,
+		input:    ioInput,
 	}
 	go startIo(p, ioChannels)
 }
