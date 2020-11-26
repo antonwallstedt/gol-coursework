65cf5a7671c3daa7ec23cd3fdcf06817ec739b4a
lybunvath <lybunvath@gmail.com>
2020-11-26 18:12:45 +0700

    distributor.go
    I have add a worker function to make our program run in many threads.

--- parallel/gol/distributor.go
+++ parallel/gol/distributor.go
@@ -46,20 +46,53 @@ func aliveNeighbour(p Params, y, x int, world [][]byte) int {
 
 	return a
 }
-func swappinng(oldWorld [][]byte, newWorld [][]byte) {
-	x := oldWorld
-	oldWorld = newWorld
+
+func worker(p Params, startY, endY, endX int, world [][]byte, newWorld [][]byte, out chan []util.Cell, turn int, turns int) {
+	var liveCell []util.Cell
+	realWorkerHeight := endY - startY
+	for y := 0; y < realWorkerHeight; y++ {
+		for x := 0; x < endX; x++ {
+			a := aliveNeighbour(p, y, x, world)
+			if world[y][x] == Alive {
+				if a == 2 || a == 3 {
+					newWorld[y][x] = Alive
+					// listCell = append(listCell, util.Cell{X: x, Y: y})
+
+				} else {
+					newWorld[y][x] = Dead
+
+				}
+			} else {
+				if a == 3 {
+					newWorld[y][x] = Alive
+					// listCell = append(listCell, util.Cell{X: x, Y: y})
+				} else {
+					newWorld[y][x] = Dead
+				}
+			}
+		}
+	}
+	x := world
+	world = newWorld
 	newWorld = x
+
+	for y := 0; y < realWorkerHeight; y++ {
+		for x := 0; x < endX; x++ {
+			if world[y][x] == Alive {
+				liveCell = append(liveCell, util.Cell{X: x, Y: y})
+			}
+		}
+	}
+	out <- liveCell
+
 }
 
 // distributor divides the work between workers and interacts with other goroutines.
 func distributor(p Params, c distributorChannels) {
 	c.ioCommand <- ioInput
 	c.ioFileName <- fmt.Sprintf("%vx%v", p.ImageHeight, p.ImageWidth)
-	var Cell util.Cell
-	var listCell []util.Cell
-	var CellFlip CellFlipped
-	var TurnComplete TurnComplete
+	// var CellFlip CellFlipped
+	// var TurnComplete TurnComplete
 	var FinalTurnComplete FinalTurnComplete
 
 	// TODO: Create a 2D slice to store the world.
@@ -71,69 +104,81 @@ func distributor(p Params, c distributorChannels) {
 	for i := range newWorld {
 		newWorld[i] = make([]byte, p.ImageWidth)
 	}
-	// TODO: For all initially alive cells send a CellFlipped Event.
+	workerHeight := p.ImageHeight / 16
+	out := make([]chan []util.Cell, 16)
+	for i := range out {
+		out[i] = make(chan []util.Cell)
+	}
 	for y := 0; y < p.ImageHeight; y++ {
 		for x := 0; x < p.ImageWidth; x++ {
 			input := <-c.ioInput
 			world[y][x] = input
 		}
 	}
+
+	// TODO: For all initially alive cells send a CellFlipped Event.
 	turn := 0
+	var newLiveCell []util.Cell
 	for turn < p.Turns {
-		for y := 0; y < p.ImageHeight; y++ {
-			for x := 0; x < p.ImageWidth; x++ {
-				a := aliveNeighbour(p, y, x, world)
-				if world[y][x] == Alive {
-					if a == 2 || a == 3 {
-						newWorld[y][x] = Alive
-						// listCell = append(listCell, util.Cell{X: x, Y: y})
 
-					} else {
-						newWorld[y][x] = Dead
-						Cell.X = x
-						Cell.Y = y
-						CellFlip.Cell = Cell
-						CellFlip.CompletedTurns = turn
-						c.events <- CellFlip
+		for i := 0; i < 16; i++ {
+			go worker(p, i*workerHeight, (i+1)*workerHeight, p.ImageWidth, world, newWorld, out[i], turn, p.Turns)
+		}
+		for i := 0; i < 16; i++ {
+			part := <-out[i]
+			newLiveCell = append(newLiveCell, part...)
 
-					}
-				} else {
-					if a == 3 {
-						newWorld[y][x] = Alive
-						Cell.X = x
-						Cell.Y = y
-						CellFlip.Cell = Cell
-						CellFlip.CompletedTurns = turn
-						c.events <- CellFlip
-						// listCell = append(listCell, util.Cell{X: x, Y: y})
-
-					} else {
-						newWorld[y][x] = Dead
-					}
-				}
-			}
 		}
 
+		// for y := 0; y < p.ImageHeight; y++ {
+		// 	for x := 0; x < p.ImageWidth; x++ {
+		// 		a := aliveNeighbour(p, y, x, world)
+		// 		if world[y][x] == Alive {
+		// 			if a == 2 || a == 3 {
+		// 				newWorld[y][x] = Alive
+		// 				// listCell = append(listCell, util.Cell{X: x, Y: y})
+
+		// 			} else {
+		// 				newWorld[y][x] = Dead
+		// 				Cell.X = x
+		// 				Cell.Y = y
+		// 				CellFlip.Cell = Cell
+		// 				CellFlip.CompletedTurns = turn
+		// 				c.events <- CellFlip
+
+		// 			}
+		// 		} else {
+		// 			if a == 3 {
+		// 				newWorld[y][x] = Alive
+		// 				Cell.X = x
+		// 				Cell.Y = y
+		// 				CellFlip.Cell = Cell
+		// 				CellFlip.CompletedTurns = turn
+		// 				c.events <- CellFlip
+		// 				// listCell = append(listCell, util.Cell{X: x, Y: y})
+
+		// 			} else {
+		// 				newWorld[y][x] = Dead
+		// 			}
+		// 		}
+		// 	}
+		// }
+
+		// turn++
+		// x := world
+		// world = newWorld
+		// newWorld = x
+
+		// TurnComplete.CompletedTurns = turn
+		// c.events <- TurnComplete
 		turn++
-		x := world
-		world = newWorld
-		newWorld = x
-
-		TurnComplete.CompletedTurns = turn
-		c.events <- TurnComplete
 
 	}
-
-	for y := 0; y < p.ImageHeight; y++ {
-		for x := 0; x < p.ImageWidth; x++ {
-			if world[y][x] == Alive {
-				listCell = append(listCell, util.Cell{X: x, Y: y})
-			}
-		}
+	if turn == p.Turns {
+		FinalTurnComplete.Alive = newLiveCell
+		FinalTurnComplete.CompletedTurns = turn
+		c.events <- FinalTurnComplete
 	}
-	FinalTurnComplete.Alive = listCell
-	FinalTurnComplete.CompletedTurns = turn
-	c.events <- FinalTurnComplete
 
 	// TODO: Execute all turns of the Game of Life.
 	// TODO: Send correct Events when required, e.g. CellFlipped, TurnComplete and FinalTurnComplete.
