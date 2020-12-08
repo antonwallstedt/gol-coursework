8c7e7eb5e6252515e7203eaf24030351af36e74d
antonwallstedt <antonwallstedt@outlook.com>
2020-12-08 11:57:44 +0000

    Working on engine

---
+++ distributed/engine/engine.go
@@ -0,0 +1,94 @@
+package engine
+
+import (
+	"uk.ac.bris.cs/gameoflife/gol"
+	"uk.ac.bris.cs/gameoflife/stubs"
+	"uk.ac.bris.cs/gameoflife/util"
+)
+
+const ALIVE = 255
+const DEAD = 0
+
+func makeWorld(height, width int) [][]byte {
+	world := make([][]byte, height)
+	for i := range world {
+		world[i] = make([]byte, width)
+	}
+	return world
+}
+
+func mod(x, m int) int {
+	return (x + m) % m
+}
+
+func calculateNeighbours(x, y int, world [][]byte) int {
+	neighbours := 0
+	height := len(world)
+	width := len(world[0])
+	for i := -1; i <= 1; i++ {
+		for j := -1; j <= 1; j++ {
+			if i != 0 || j != 0 {
+				if world[mod(y+i, height)][mod(x+j, width)] == ALIVE {
+					neighbours++
+				}
+			}
+		}
+	}
+	return neighbours
+}
+
+func calculateNextState(p gol.Params, world [][]byte) [][]byte {
+	height := len(world)
+	width := p.ImageWidth
+	newWorld := makeWorld(height, width)
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			neighbours := calculateNeighbours(x, y, world)
+			if world[y][x] == ALIVE {
+				if neighbours == 2 || neighbours == 3 {
+					newWorld[y][x] = ALIVE
+				} else {
+					newWorld[y][x] = DEAD
+				}
+			} else {
+				if neighbours == 3 {
+					newWorld[y][x] = ALIVE
+				} else {
+					newWorld[y][x] = DEAD
+				}
+			}
+		}
+	}
+	return newWorld
+}
+
+func calculateAliveCells(p gol.Params, world [][]byte) []util.Cell {
+	aliveCells := []util.Cell{}
+	for y := 0; y < p.ImageHeight; y++ {
+		for x := 0; x < p.ImageWidth; x++ {
+			if world[y][x] == ALIVE {
+				aliveCells = append(aliveCells, util.Cell{X: x, Y: y})
+			}
+		}
+	}
+	return aliveCells
+}
+
+type Work struct {
+	params gol.Params
+	world  [][]byte
+}
+
+func gameOfLife(p gol.Params, world [][]byte) {
+	turn := 0
+	for turn < p.Turns {
+		world = calculateNextState(p, world)
+		turn++
+	}
+}
+
+type Engine struct{}
+
+func (e *Engine) GameOfLife(req stubs.Request) {
+
+}
