85e3e825b6d52230a742ed135862ed4b9c842e30
antonwallstedt <antonwallstedt@outlook.com>
2020-12-08 13:21:28 +0000

    Edited benchmarking

--- parallel/parallel_test.go
+++ parallel/parallel_test.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"uk.ac.bris.cs/gameoflife/gol"
 )
 
 const (
-	turns       = 1000
+	turns       = 250
 	imageHeight = 512
 	imageWidth  = 512
 )
@@ -18,17 +19,21 @@ func Benchmark(b *testing.B) {
 	for threads := 1; threads <= 16; threads++ {
 		params.Threads = threads
 		testName := fmt.Sprintf("%dx%dx%d-%d", params.ImageWidth, params.ImageHeight, params.Turns, params.Threads)
-		b.Run(testName, func(b *testing.B) {
-			b.StartTimer()
-			events := make(chan gol.Event)
-			gol.Run(params, events, nil)
-			for event := range events {
-				switch event.(type) {
-				case gol.FinalTurnComplete:
-					break
+		os.Stdout = nil
+		for i := 0; i < b.N; i++ {
+			b.Run(testName, func(b *testing.B) {
+				b.StartTimer()
+				events := make(chan gol.Event)
+				gol.Run(params, events, nil)
+				for event := range events {
+					switch event.(type) {
+					case gol.FinalTurnComplete:
+						break
+					}
 				}
-			}
-			b.StopTimer()
-		})
+				b.StopTimer()
+			})
+		}
+
 	}
 }
