7cd2ac17f1de8da90ee9ac3b2a4878e182892f7c
antonwallstedt <antonwallstedt@outlook.com>
2020-12-18 09:49:30 +0000

    Formatting and changing IP

--- distributed/gol/controller.go
+++ distributed/gol/controller.go
@@ -130,7 +130,7 @@ func controller(p Params, c controllerChannels) {
 	if flag.Lookup("server") != nil {
 		serverIP = flag.Lookup("server").Value.String()
 	} else {
-		serverIP = "35.173.57.41:8030"
+		serverIP = "3.80.232.44:8030"
 	}
 	client, _ := rpc.Dial("tcp", serverIP)
 
@@ -183,9 +183,9 @@ func controller(p Params, c controllerChannels) {
 			select {
 			case <-ticker.C:
 				aliveCells := requestAliveCells(*client)
-				// If the number of completed turns by the engine are close to the total number of turns to be completed,
+				// If the number of completed turns by the engine is close to the total number of turns to be completed,
 				// stop the ticker so it doesn't make another RPC call, and make a RPC call to request the results from the engine.
-				if p.Turns-aliveCells.CompletedTurns <= 5 {
+				if p.Turns-aliveCells.CompletedTurns <= 60 {
 					ticker.Stop()
 					go requestResults(*client, resultsChan)
 				} else {
